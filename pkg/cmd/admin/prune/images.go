0f5ca87b6c32cfee1d8751cac36a4f6e15788c0c
Andy Goldstein <agoldste@redhat.com>
2015-06-02 14:16:06 -0400

    Require token or service account for image pruning

--- pkg/cmd/admin/prune/images.go
+++ pkg/cmd/admin/prune/images.go
@@ -2,6 +2,7 @@ package prune
 
 import (
 	"crypto/x509"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -18,6 +19,7 @@ import (
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
 	"github.com/golang/glog"
+	"github.com/openshift/origin/pkg/client"
 	"github.com/openshift/origin/pkg/cmd/util/clientcmd"
 	imageapi "github.com/openshift/origin/pkg/image/api"
 	"github.com/openshift/origin/pkg/image/prune"
@@ -34,6 +36,7 @@ type pruneImagesConfig struct {
 	KeepYoungerThan    time.Duration
 	TagRevisionsToKeep int
 	CABundle           string
+	ServiceAccountName string
 }
 
 func NewCmdPruneImages(f *clientcmd.Factory, parentName, name string, out io.Writer) *cobra.Command {
@@ -50,10 +53,10 @@ func NewCmdPruneImages(f *clientcmd.Factory, parentName, name string, out io.Wri
 
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) > 0 {
-				glog.Fatalf("No arguments are allowed to this command")
+				glog.Fatal("No arguments are allowed to this command")
 			}
 
-			osClient, kClient, err := f.Clients()
+			osClient, kClient, registryClient, err := getClients(f, cfg)
 			cmdutil.CheckErr(err)
 
 			allImages, err := osClient.Images().List(labels.Everything(), fields.Everything())
@@ -137,30 +140,6 @@ func NewCmdPruneImages(f *clientcmd.Factory, parentName, name string, out io.Wri
 				manifestPruneFunc    prune.ManifestPruneFunc
 			)
 
-			// get the client config so we can get the TLS config
-			clientConfig, err := f.OpenShiftClientConfig.ClientConfig()
-			cmdutil.CheckErr(err)
-
-			tlsConfig, err := kclient.TLSConfigFor(clientConfig)
-			cmdutil.CheckErr(err)
-
-			// if the user specified a CA on the command line, add it to the
-			// client config's CA roots
-			if len(cfg.CABundle) > 0 {
-				data, err := ioutil.ReadFile(cfg.CABundle)
-				cmdutil.CheckErr(err)
-				if tlsConfig.RootCAs == nil {
-					tlsConfig.RootCAs = x509.NewCertPool()
-				}
-				tlsConfig.RootCAs.AppendCertsFromPEM(data)
-			}
-
-			registryClient := &http.Client{
-				Transport: &http.Transport{
-					TLSClientConfig: tlsConfig,
-				},
-			}
-
 			switch cfg.DryRun {
 			case false:
 				imagePruneFunc = func(image *imageapi.Image) error {
@@ -198,6 +177,157 @@ func NewCmdPruneImages(f *clientcmd.Factory, parentName, name string, out io.Wri
 	cmd.Flags().DurationVar(&cfg.KeepYoungerThan, "keep-younger-than", cfg.KeepYoungerThan, "Specify the minimum age of a build for it to be considered a candidate for pruning.")
 	cmd.Flags().IntVar(&cfg.TagRevisionsToKeep, "keep-tag-revisions", cfg.TagRevisionsToKeep, "Specify the number of image revisions for a tag in an image stream that will be preserved.")
 	cmd.Flags().StringVar(&cfg.CABundle, "certificate-authority", cfg.CABundle, "The path to a certificate authority bundle to use when communicating with the OpenShift-managed registries. Defaults to the certificate authority data from the current user's config file.")
+	cmd.Flags().StringVar(&cfg.ServiceAccountName, "service-account", cfg.ServiceAccountName, "If specified, use this service account to perform all pruning operations.")
 
 	return cmd
+}
+
+func getClients(f *clientcmd.Factory, cfg *pruneImagesConfig) (*client.Client, *kclient.Client, *http.Client, error) {
+	clientConfig, err := f.OpenShiftClientConfig.ClientConfig()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	var (
+		token          string
+		osClient       *client.Client
+		kClient        *kclient.Client
+		registryClient *http.Client
+	)
+
+	switch {
+	case len(cfg.ServiceAccountName) > 0:
+		ns, err := f.DefaultNamespace()
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		saName := cfg.ServiceAccountName
+		if saParts := strings.Split(saName, "/"); len(saParts) > 1 {
+			ns = saParts[0]
+			saName = saParts[1]
+		}
+
+		_, kClient, err = f.Clients()
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		sa, err := kClient.ServiceAccounts(ns).Get(saName)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		for _, secretRef := range sa.Secrets {
+			secret, err := kClient.Secrets(ns).Get(secretRef.Name)
+			if err != nil {
+				// might be a dead reference
+				continue
+			}
+
+			if secret.Type != kapi.SecretTypeServiceAccountToken {
+				continue
+			}
+			if secret.Annotations[kapi.ServiceAccountNameKey] != sa.Name {
+				continue
+			}
+			if secret.Annotations[kapi.ServiceAccountUIDKey] != string(sa.UID) {
+				continue
+			}
+			if len(secret.Data[kapi.ServiceAccountTokenKey]) == 0 {
+				continue
+			}
+
+			// copy the config
+			saClientConfig := *clientConfig
+
+			// set the token
+			token = string(secret.Data[kapi.ServiceAccountTokenKey])
+			saClientConfig.BearerToken = token
+
+			// clear out everything else
+			saClientConfig.CertFile = ""
+			saClientConfig.CertData = []byte{}
+			saClientConfig.KeyFile = ""
+			saClientConfig.KeyData = []byte{}
+			saClientConfig.Username = ""
+			saClientConfig.Password = ""
+
+			osClient, err = client.New(&saClientConfig)
+			if err != nil {
+				return nil, nil, nil, err
+			}
+
+			kClient, err = kclient.New(&saClientConfig)
+			if err != nil {
+				return nil, nil, nil, err
+			}
+		}
+
+		if len(token) == 0 {
+			err = fmt.Errorf("Unable to locate a valid token for service account %s/%s", ns, cfg.ServiceAccountName)
+			return nil, nil, nil, err
+		}
+	case len(clientConfig.BearerToken) > 0:
+		osClient, kClient, err = f.Clients()
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		token = clientConfig.BearerToken
+	default:
+		err = errors.New("You must either use a config with a token or a service account")
+		return nil, nil, nil, err
+	}
+
+	// copy the config
+	registryClientConfig := *clientConfig
+
+	// zero out everything we don't want to use
+	registryClientConfig.BearerToken = ""
+	registryClientConfig.CertFile = ""
+	registryClientConfig.CertData = []byte{}
+	registryClientConfig.KeyFile = ""
+	registryClientConfig.KeyData = []byte{}
+
+	// we have to set a username to something for the Docker login
+	// but it's not actually used
+	registryClientConfig.Username = "unused"
+
+	// set the "password" to be the token
+	registryClientConfig.Password = token
+
+	tlsConfig, err := kclient.TLSConfigFor(&registryClientConfig)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// if the user specified a CA on the command line, add it to the
+	// client config's CA roots
+	if len(cfg.CABundle) > 0 {
+		data, err := ioutil.ReadFile(cfg.CABundle)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		if tlsConfig.RootCAs == nil {
+			tlsConfig.RootCAs = x509.NewCertPool()
+		}
+
+		tlsConfig.RootCAs.AppendCertsFromPEM(data)
+	}
+
+	transport := http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+
+	wrappedTransport, err := kclient.HTTPWrappersForConfig(&registryClientConfig, &transport)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	registryClient = &http.Client{
+		Transport: wrappedTransport,
+	}
+
+	return osClient, kClient, registryClient, nil
 }
