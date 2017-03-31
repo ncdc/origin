782ca822794697b395748fba221180d5ac4792e4
Andy Goldstein <agoldste@redhat.com>
2017-03-31 11:48:26 -0400

    Add DockerShimSocket to NodeConfig so we can put in somewhere that integration tests have write permissions

--- pkg/cmd/server/kubernetes/node/node_config.go
+++ pkg/cmd/server/kubernetes/node/node_config.go
@@ -171,6 +171,7 @@ func BuildKubernetesNodeConfig(options configapi.NodeConfig, enableProxy, enable
 		server.ResolverConfig = ""
 	}
 	server.DockerExecHandlerName = string(options.DockerConfig.ExecHandlerName)
+	server.DockerShimSocket = options.DockerConfig.DockerShimSocket
 
 	if sdnapi.IsOpenShiftNetworkPlugin(server.NetworkPluginName) {
 		// set defaults for openshift-sdn
