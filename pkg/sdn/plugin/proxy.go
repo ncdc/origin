c14d8b8b4a575d9e4626bc250285f3d23ccab81b
Andy Goldstein <agoldste@redhat.com>
2017-04-04 13:24:13 -0400

    Match upstream pkg/proxy refactors

--- pkg/sdn/plugin/proxy.go
+++ pkg/sdn/plugin/proxy.go
@@ -31,7 +31,7 @@ type OsdnProxy struct {
 
 	lock         sync.Mutex
 	firewall     map[string][]proxyFirewallItem
-	allEndpoints []kapi.Endpoints
+	allEndpoints []*kapi.Endpoints
 }
 
 // Called by higher layers to create the proxy plugin instance; only used by nodes
@@ -116,7 +116,7 @@ func (proxy *OsdnProxy) firewallBlocksIP(namespace string, ip net.IP) bool {
 	return false
 }
 
-func (proxy *OsdnProxy) OnEndpointsUpdate(allEndpoints []kapi.Endpoints) {
+func (proxy *OsdnProxy) OnEndpointsUpdate(allEndpoints []*kapi.Endpoints) {
 	proxy.lock.Lock()
 	defer proxy.lock.Unlock()
 	proxy.allEndpoints = allEndpoints
@@ -129,7 +129,7 @@ func (proxy *OsdnProxy) updateEndpoints() {
 		return
 	}
 
-	filteredEndpoints := make([]kapi.Endpoints, 0, len(proxy.allEndpoints))
+	filteredEndpoints := make([]*kapi.Endpoints, 0, len(proxy.allEndpoints))
 
 EndpointLoop:
 	for _, ep := range proxy.allEndpoints {
